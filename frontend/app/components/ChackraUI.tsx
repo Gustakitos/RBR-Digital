c46b527556c50e5c0bf10e914d9898424f68e12e
Luis Gustavo <luisgustavolg1033@gmail.com>
2024-05-17 18:53:34 -0300

    add navbar to front end project, add chakra icons

---
+++ frontend/app/components/ChackraUI.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+export {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+export {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from '@chakra-ui/icons';
